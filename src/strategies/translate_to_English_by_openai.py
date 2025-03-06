c99a51a20ee232a0e13868074680074a0eb68f03
Danchitomoo <taka.2000kako@gmail.com>
2025-03-07 07:57:32 +0900

    Debug: remove unintentional blank and line breaks in translate to English by openai

--- src/strategies/translate_to_English_by_openai.py
+++ src/strategies/translate_to_English_by_openai.py
@@ -9,26 +9,22 @@ class TranslateToEnglishPrompt(ABC):
 
 class BasicTranslateToEnglishPrompt(TranslateToEnglishPrompt):
     prompt_name = "basic"
-    system_prompt = """
-    You have to translate sentence below to English which
-    must be quite plain and easy to understand.
-    If the terminology is too difficult, you must add explanation for that words.
-    """
+    system_prompt = "You have to translate sentence below to English which\
+    must be quite plain and easy to understand.\
+    If the terminology is too difficult, you must add explanation for that words."
 
 class OptimizedTranslateToEnglishPrompt1(TranslateToEnglishPrompt):
     prompt_name = "optimized_1"
-    system_prompt = """
-    you need an English translator, spell checker, and veterinary medical language
-    expert who can translate Japanese text into English.The translation must be
-    improved and simplified to make it easier to understand for non-specialists at
-    a high school level. My request is to keep the meaning intact, but with a more
-    literal translation. Notably, you need describe the specific name of country if
-    necessary. Especially you should use "in Japan" instead of "in our country".
-    Restrictions:
-    - The response must be only one translation, containiing only corrections and
-    improvements to the Japanese text, not notes or anythins else.
-    - Set tempreture = 0
-    """
+    system_prompt = "you need an English translator, spell checker, and veterinary medical language\
+    expert who can translate Japanese text into English.The translation must be\
+    improved and simplified to make it easier to understand for non-specialists at\
+    a high school level. My request is to keep the meaning intact, but with a more\
+    literal translation. Notably, you need describe the specific name of country if\
+    necessary. Especially you should use 'in Japan' instead of 'in our country'.\
+    Restrictions:\
+    - The response must be only one translation, containiing only corrections and\
+    improvements to the Japanese text, not notes or anythins else.\
+    - Set tempreture = 0"
 
 async def translate_to_English_by_openai(
     openai_client: OpenAIClient,
